6dc2a66646b43de62afd35b7d287b8ea8153452a
Huang Yonggang <huangyg@mars22.com>
2015-07-25 00:02:58 +0800

    new item object

--- tool/PSMD.auto.js
+++ tool/PSMD.auto.js
@@ -10,47 +10,33 @@ function postfile(item) {
 	var thisHash = infra.getthisHash(__filename,3);
 	console.log("enter PSMD postfile, ID:",thisHash);
 	
-	var pubfile = new Object();
-	var files = fs.readdirSync("post/");
-
-	files.forEach(function(filename) {
-		if(filename.substr(0,4) == "nor."){
-			var nor = yaml.safeLoad(fs.readFileSync("post/"+filename,'utf8'));
-			var pubkey = openpgp.key.readArmored(nor.data.pubkey).keys[0];
-			pubfile[pubkey.primaryKey.fingerprint] = "post/"+filename;
-		}else if((filename.substr(filename.indexOf(".")+1,5) == "auto.") || (filename.substr(0,5) == "auto.")){
-			var auto = yaml.safeLoad(fs.readFileSync("post/"+filename,'utf8'));
-			pubfile[auto.data.id] = "post/"+filename;
-		}
-	});
-	
-	if (item.substr(0,9) == "transfer."){
-		var obj = yaml.safeLoad(fs.readFileSync("post/"+item, 'utf8'));
-		
+	if (item.filename.substr(0,9) == "transfer."){
+		var obj = yaml.safeLoad(item.content);
+		//console.log("\npostupdate event item:\n",item);
+		var data ;
 		if(obj.log != undefined){
 			var log = yaml.safeLoad(obj.log);
 			data = yaml.safeLoad(log.data);
-		}else if (obj.sigtype === 0){
-				data = obj.data;
-		}else{
+		}else if (obj.sigtype == 0){
+			data = obj.data;
+		}else if (obj.sigtype == 2){
 			data = obj.data;
 			var msg = openpgp.cleartext.readArmored(data);
 			var author = obj.author ;
-			var nor = yaml.safeLoad(fs.readFileSync(pubfile[author],'utf8'));
+			var nor = yaml.safeLoad(fs.readFileSync(infra.key[author].norfilename,'utf8'));
 			var pubkeys = openpgp.key.readArmored(nor.data.pubkey).keys;
 			var pubkey = pubkeys[0];
 			var result = msg.verify(pubkeys);
-
 			data = yaml.safeLoad(msg.text);
 		}
-		
-		var input = data.input;
-		var output = data.output;
-		
-		var id = output.id;
-		if (id == thisHash) {
+		if(data.hasOwnProperty("output")) {
+			var output = data.output;
+			var id = output.id;
+			var amount = output.amount;
+			if (id == thisHash) {
 			var amount = output.amount;
 			infra.CODtransfer(thisHash,'7c0fa6e0fff49e7d0b15a112cef2e8969dd42966',amount*0.02);
+		}
 		}
 	}
 }
